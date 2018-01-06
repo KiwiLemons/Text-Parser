5b3c254ef6f6caaa75b8926b2b26fa597af03dc9
KiwiLemons <lemonzyyz@gmail.com>
2018-01-05 22:42:57 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace TextParser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.Prefix = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.Suffix = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(350, 41);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Parse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Left String:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(221, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(65, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Right String:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 15);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(97, 43);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(237, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(292, 15);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(133, 20);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 46);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Extracted Text:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 82);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Input:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(10, 101);
+            this.textBox4.Multiline = true;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox4.Size = new System.Drawing.Size(565, 303);
+            this.textBox4.TabIndex = 8;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(350, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 27);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Input From File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox5.Location = new System.Drawing.Point(10, 410);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(565, 22);
+            this.textBox5.TabIndex = 10;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(436, 45);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(74, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Recursive";
+            this.toolTip1.SetToolTip(this.checkBox1, "When checked, the parser will extract every instance of a match.\r\nIf un-checked, " +
+        "the parser will stop checking for matches after one match.");
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.AutoPopDelay = 20000;
+            this.toolTip1.InitialDelay = 500;
+            this.toolTip1.ReshowDelay = 100;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(442, 13);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Example";
+            this.toolTip1.SetToolTip(this.button3, "Does an example parse so you can understand how to use the program");
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(468, 68);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(107, 27);
+            this.button4.TabIndex = 13;
+            this.button4.Text = "Input from URL";
+            this.toolTip1.SetToolTip(this.button4, "Downloads the source of the webpage that you input");
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(516, 46);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(68, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Numbers";
+            this.toolTip1.SetToolTip(this.checkBox2, "When enabled it puts numbers on every new line in a recursive parse");
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(52, 75);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(36, 13);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Prefix:";
+            // 
+            // Prefix
+            // 
+            this.Prefix.Location = new System.Drawing.Point(97, 72);
+            this.Prefix.Name = "Prefix";
+            this.Prefix.Size = new System.Drawing.Size(100, 20);
+            this.Prefix.TabIndex = 15;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(203, 75);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(33, 13);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "Suffix";
+            // 
+            // Suffix
+            // 
+            this.Suffix.Location = new System.Drawing.Point(242, 72);
+            this.Suffix.Name = "Suffix";
+            this.Suffix.Size = new System.Drawing.Size(99, 20);
+            this.Suffix.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(587, 442);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.Suffix);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.Prefix);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text Parser Example";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox Prefix;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox Suffix;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Net;
+using Microsoft.VisualBasic;
+
+namespace TextParser
+{
+    public partial class Form1 : Form
+    {
+        WebClient webClient = new WebClient();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Parse Button
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Check if boxes are empty
+            if (textBox1.Text == "" || textBox3.Text == "" || textBox4.Text == "")
+                textBox5.Text = "Please do not leave the \"Left String\", \"Right String\", or the \"Input\" fields empty";
+            else if (textBox1.Text == textBox3.Text)
+                textBox5.Text = "The left and right string cannot match";
+            else
+            {
+                if (!checkBox1.Checked)
+                {
+                    textBox2.Text = TextParser(textBox1.Text, textBox3.Text, textBox4.Text, Prefix.Text, Suffix.Text, checkBox1.Checked, checkBox2.Checked);
+                }
+                else
+                {
+                    textBox2.Text = "It has been copied to your Clipboard";
+                    Clipboard.SetText(TextParser(textBox1.Text, textBox3.Text, textBox4.Text, Prefix.Text, Suffix.Text, checkBox1.Checked, checkBox2.Checked));
+                }
+                
+            }
+        }
+        private string TextParser(string leftString, string rightString, string Input, string prefix, string suffix, bool recursive,bool numbers)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+            string extracted = "error";
+            if (!recursive)
+            {
+                try
+                {
+                    extracted = String.Format("{1}{0}{2}", Input.Substring(Input.IndexOf(leftString) + leftString.Length, Input.IndexOf(rightString) - Input.IndexOf(leftString) - leftString.Length),prefix,suffix);
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    textBox5.Text = ex.Message;
+                }
+            }
+            else if (recursive)
+            {
+                Regex counter = new Regex(String.Format("{0}.*{1}", leftString, rightString));
+                int amount = counter.Matches(Input).Count;
+                int i = 1, errors = 0;
+                extracted = "";
+                while (i <= amount)
+                {
+                    try
+                    {
+                        if (numbers)
+                        {
+                            extracted += String.Format("#{0} {2}{1}{3}\n", i - errors, Input.Substring(Input.IndexOf(leftString) + leftString.Length, Input.IndexOf(rightString) - Input.IndexOf(leftString) - leftString.Length), prefix, suffix);
+                        }
+                        else
+                        {
+                            extracted += String.Format("{1}{0}{2}\n",Input.Substring(Input.IndexOf(leftString) + leftString.Length, Input.IndexOf(rightString) - Input.IndexOf(leftString) - leftString.Length), prefix, suffix);
+                        }
+                    }
+                    catch (ArgumentOutOfRangeException ex)
+                    {
+                        //textBox5.Text = ex.Message;
+                        ++errors;
+                    }
+                    Input = Input.Substring(Input.IndexOf(rightString) + rightString.Length);
+                    ++i;
+                }
+            }
+            stopwatch.Stop();
+            textBox5.Text = String.Format("Time elapsed:  {0} ms", stopwatch.ElapsedMilliseconds.ToString());
+            return extracted;
+        }
+        //Input from File button
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fileDialog = new OpenFileDialog();
+            fileDialog.Filter = ".txt files | *.txt";
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                textBox4.Text = File.ReadAllText(fileDialog.FileName);
+                textBox5.Text = String.Format("Imported file  {0}", fileDialog.FileName);
+            }
+        }
+        //Example button
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "///";
+            textBox3.Text = @"\\\";
+            textBox4.Text = @"///find me\\\";
+            Prefix.Text = "You can ";
+            Suffix.Text = "!";
+            textBox2.Text = TextParser(textBox1.Text, textBox3.Text, textBox4.Text,Prefix.Text,Suffix.Text, false,false);
+        }
+        //input from URL button
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string url = Interaction.InputBox("Enter the URL into the textbox", "URL Input", "");
+            if (url != "")
+            {
+                try
+                {
+                    textBox4.Text = webClient.DownloadString(url);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+        //Recusive checkbox state change
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            checkBox2.Enabled = checkBox1.Checked;
+        }
+    }
+}
